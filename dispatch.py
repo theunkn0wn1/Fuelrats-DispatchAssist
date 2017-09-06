3ec0789a3ad1bbf69b1a442bce079d1b811cd340
Joshua <theunkn0wn1@users.noreply.github.com>
2017-09-06 11:04:29 -0700

    Implemented inject-a-case handling. Can now detect when a case is injected into existence. /inject is now just a proxy for !inject, no fancy handling

--- dispatch.py
+++ dispatch.py
@@ -39,6 +39,24 @@ def log(trace, msg):
     print("[{Stack}:{trace}]\t {message}".format(Stack=__module_name__, message=msg, trace=trace))
 
 
+def on_message_received(*args):
+    phrase = args[0]
+    channel = phrase[2]
+    sender = phrase[0]
+    try:
+        if channel != '#RatChat' and channel != '#ratchat':
+            log('on_message_received', 'sender = {sender}\nData= {data}'.format(sender=sender, data=phrase))
+        elif sender == ':DrillSqueak[BOT]!sopel@bot.fuelrats.com':
+            log("on_message_received", "sender is correct, attempting parse...")
+            Tracker.inject([None, None, None], True, phrase)
+        else:
+            pass
+            # log("on_message_received", 'not the expected user')
+    except Exception as e:
+        log("on_message_received", 'some error occurred! Error is as follows:]\n{}'.format(e))
+    return hc.EAT_PLUGIN
+
+
 class Tracker:
     """Handles case storage and handling"""
     def __init__(self):
@@ -53,8 +71,10 @@ class Tracker:
     @staticmethod
     def inject(list_arguments, is_capture=False, capture_data=None):
         """Generates a new case via  !inject"""
-        if len(list_arguments) < 3:
+        if list_arguments is None or len(list_arguments) < 3:
             return -1  # not enough arguments
+        elif is_capture:
+            pass
         else:
             inject_args = {'client': list_arguments[1], 'system': list_arguments[3], 'platform': list_arguments[2]}
             # order: client, platform, system
@@ -70,31 +90,47 @@ class Tracker:
             elif capture[0] != ':DrillSqueak[BOT]!sopel@bot.fuelrats.com':
                 log("on_message_captured"," invalid capture event")
             else:
+                log("on_message_captured", 'Beginning parse attempt ZZZZzzzZz...')
                 i = 0
-                client = inject_args['client']
-                platform = inject_args['system']
+                # client = inject_args['client']
+                # platform = inject_args['system']
                 case = -1
+                log("step1", 'completed!')
                 for phrase in capture:
-                    if phrase == 'PC' or phrase == 'PS4' or phrase == 'XB':
+                    if phrase == 'PC)' or phrase == 'PS4)' or phrase == 'XB)':
+                        log('step2', 'searching for platform...')
                         platform = phrase
+                        log('step2', 'platform is {}'.format(platform))
                     elif phrase[:5] == 'case':
+                        log('step3', 'looking for client name...')
                         parsed_string = capture[i-1]  # phrase before is the client name
                         parsed_string = parsed_string[:len(parsed_string)-2]  # strip the 's from the end
                         parsed_string = parsed_string.strip(':')  # and the : from the start
                         client = parsed_string  # And we have our product!
+                        log('step3', 'client is {}'.format(client))
                     elif phrase == "(Case":
+                        log('step4', 'searching for CaseID...')
                         case = capture_data[i+1].strip(',')
-
+                        log('step4', 'cid is {CID}'.format(CID = case))
+                    else:
+                        # log("failed:", "word not read: {}".format(phrase))
+                        pass
                     i += 1
                 log("on_message_captured", "append({},{},{},{},{})".format(case, client, platform, False, 'En-us'))
                 temp_dict = {'case': case, 'platform': platform, 'cr': False, 'lang': 'EN-us',
                              'client': client, 'system': 'Sol'}
                 Tracker.append(temp_dict)
+            log("miss?","miss?")
         if is_capture:
             if capture_data is None or capture_data == "":
                 log("Tracker", "[FAIL]\t capture_data was None or was blank")
             else:
-                on_message_captured(capture_data)
+                log('is_capture_check', 'PASSED')
+                try:
+                    on_message_captured(capture_data)
+                except Exception as e:
+                    log("[FATAL]", "an error occured as follows:\n {error}".format(e))
+        else:
             log("Tracker", "not capture data")
 
     @staticmethod
@@ -140,7 +176,7 @@ class Commands:
         log("run_tests", "Running Tracker.inject Test 1...")
         Tracker.inject(None, True, None)
         log("run_tests", "running test 2")
-        Tracker.inject([], True, x)
+        print(Tracker.inject([None, 'clientName', 'systemName', 'PC'], True, x))
         log("run_tests", "done!")
 
     @staticmethod
@@ -253,7 +289,7 @@ def init():
         "inject": cmd.inject_case,
         "dClear": cmd.clear_drill,
         "stage": cmd.stage,
-        "new": board.append,
+        # "new": board.append,  # yeah no, this needs to be put into a wrapper (expects dict, gets string array)
         "del": board.rm,
         'rm': board.rm,
         "readout": board.readout
@@ -262,6 +298,9 @@ def init():
         for key in commands:
             log("init", "adding hook for\t{}".format(key))
             hc.hook_command(key, commands[key])
+            hc.hook_server("PRIVMSG", on_message_received)
+    except Exception as e:
+        log("init")
     # hc.hook_print("Channel Message",cmd.print_hook)
     # hc.hook_print("Beep",cmd.print_hook)
     # hc.hook_print("Generic message",cmd.generic_msg)
